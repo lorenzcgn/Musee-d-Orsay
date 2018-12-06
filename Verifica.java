6814050202e34fc3c97053470b342749e069c17e
Lorenzo Cagni <45255725+lorenzocagni@users.noreply.github.com>
2018-12-06 08:17:24 +0100

    Add files via upload

---
+++ Verifica.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package verifica;
+
+/**
+ *
+ * @author 75844087
+ */
+public class Verifica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
